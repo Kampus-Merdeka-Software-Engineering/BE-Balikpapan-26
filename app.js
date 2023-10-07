021c676cecd840d6fb37c0c90e4bb8361912b242
arzil12 <arzilfri@gmail.com>
2023-10-07 11:55:42 +0700

    rework app.js

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+const router = express.Router();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    const url = req.url;
-    const method = req.method;
-    console.log(`${method}: ${url}`);
-    return next();
+    console.log(`Method: ${req.method} ${req.path}`);
+    next();
 });
 
 routes.forEach((route) => app.use(route));
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Internal server error' });
-});
-
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
